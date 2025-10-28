f3ee362098384e8390a9ac52a3e117f6d28ce982
Ashlee <ashleeramirezrosario@gmail.com>
2025-10-27 22:08:19 -0400

    Arreglo en el context del repo generico

---
+++ PetCareApp.Infraestructure.Persistence/Repositories/CitaRepository.cs
@@ -0,0 +1,20 @@
+﻿using PetCareApp.Core.Domain.Entities;
+using PetCareApp.Core.Domain.Interfaces;
+using PetCareApp.Infraestructure.Persistence.Context;
+
+namespace PetCareApp.Infraestructure.Persistence.Repositories
+{
+    public class CitaRepository : GeneRepositorio<Cita>, ICitaRepository
+    {
+        private readonly PetCareContext _context;
+
+        public CitaRepository(PetCareContext context) : base(context)
+        {
+            _context = context;
+        }
+        public Task<List<Cita?>> GetCitasByDate(DateOnly date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
